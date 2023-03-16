1cf5285d15a9553c3582cc84c2ba5cd9c065d1fb
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 20:39:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React from "react";
-import "../styles/App.css";
-import Component from "./Component";
-import ErrorBoundary from "./ErrorBoundary";
+import React, { useState } from 'react';
+import '../styles/App.css';
+import Component from './Component';
+import ErrorBoundary from './ErrorBoundary';
 const App = () => {
+	const [error, setError] = useState(false);
 
-
-  return (
-
-    <div id="main">
-    <h2>React Error Boundaries</h2>
-      <ErrorBoundary>
-        <Component change={changeFunc} />
-      </ErrorBoundary>
-      <button id="gen" onClick={errorGenerator}>Generate Error</button>
-    </div>
-  );
+	const errorGenerator = () => {
+		setError(!error);
+	};
+	return (
+		<div id="main">
+			<h2>React Error Boundaries</h2>
+			<ErrorBoundary>
+				<Component error={error} />
+			</ErrorBoundary>
+			<button id="gen" onClick={errorGenerator}>
+				Generate Error
+			</button>
+		</div>
+	);
 };
 
 export default App;
